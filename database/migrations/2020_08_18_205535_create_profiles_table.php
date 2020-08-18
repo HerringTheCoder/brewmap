69d654c565d1b743a0718ecdd3dd8b3dfe43535a
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2020-08-18 23:38:49 +0200

    #11 - Added Profile model and migration

---
+++ database/migrations/2020_08_18_205535_create_profiles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'profiles'
+            , function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable;
+            $table->string('avatar_path')->nullable;
+            $table->date('birthday');
+            $table->string('facebook_path')->nullable;
+            $table->string('instagram_path')->nullable;
+            $table->string('twitter_path')->nullable;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
